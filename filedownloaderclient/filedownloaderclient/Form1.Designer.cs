ba4566397ae3b35b2354fd7410a9461240ee6982
warnov <warnov@gmail.com>
2017-08-28 00:14:33 -0500

    Automation enhanced

--- filedownloaderclient/filedownloaderclient/Form1.Designer.cs
+++ filedownloaderclient/filedownloaderclient/Form1.Designer.cs
@@ -31,12 +31,18 @@
             this.btnDownload = new System.Windows.Forms.Button();
             this.sfdMain = new System.Windows.Forms.SaveFileDialog();
             this.txtSvrPath = new System.Windows.Forms.TextBox();
-            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnAutoCopy = new System.Windows.Forms.Button();
+            this.fbdMain = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnAutoSave = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnSavingFolder = new System.Windows.Forms.Button();
+            this.lblSavingPath = new System.Windows.Forms.Label();
+            this.txtStatus = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnDownload
             // 
-            this.btnDownload.Location = new System.Drawing.Point(182, 153);
+            this.btnDownload.Location = new System.Drawing.Point(324, 143);
             this.btnDownload.Name = "btnDownload";
             this.btnDownload.Size = new System.Drawing.Size(75, 23);
             this.btnDownload.TabIndex = 0;
@@ -51,20 +57,72 @@
             this.txtSvrPath.Size = new System.Drawing.Size(362, 20);
             this.txtSvrPath.TabIndex = 1;
             // 
-            // lblStatus
+            // btnAutoCopy
             // 
-            this.lblStatus.AutoSize = true;
-            this.lblStatus.Location = new System.Drawing.Point(204, 106);
-            this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(0, 13);
-            this.lblStatus.TabIndex = 2;
+            this.btnAutoCopy.Location = new System.Drawing.Point(37, 143);
+            this.btnAutoCopy.Name = "btnAutoCopy";
+            this.btnAutoCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnAutoCopy.TabIndex = 3;
+            this.btnAutoCopy.Text = "Autocopy";
+            this.btnAutoCopy.UseVisualStyleBackColor = true;
+            this.btnAutoCopy.Click += new System.EventHandler(this.btnAutoCopy_Click);
+            // 
+            // btnAutoSave
+            // 
+            this.btnAutoSave.Location = new System.Drawing.Point(324, 235);
+            this.btnAutoSave.Name = "btnAutoSave";
+            this.btnAutoSave.Size = new System.Drawing.Size(75, 23);
+            this.btnAutoSave.TabIndex = 4;
+            this.btnAutoSave.Text = "Autosave";
+            this.btnAutoSave.UseVisualStyleBackColor = true;
+            this.btnAutoSave.Click += new System.EventHandler(this.btnAutoSave_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(37, 186);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(362, 20);
+            this.txtFileName.TabIndex = 5;
+            // 
+            // btnSavingFolder
+            // 
+            this.btnSavingFolder.Location = new System.Drawing.Point(37, 235);
+            this.btnSavingFolder.Name = "btnSavingFolder";
+            this.btnSavingFolder.Size = new System.Drawing.Size(147, 23);
+            this.btnSavingFolder.TabIndex = 6;
+            this.btnSavingFolder.Text = "Set Auto Saving Folder";
+            this.btnSavingFolder.UseVisualStyleBackColor = true;
+            this.btnSavingFolder.Click += new System.EventHandler(this.btnSavingFolder_Click);
+            // 
+            // lblSavingPath
+            // 
+            this.lblSavingPath.AutoSize = true;
+            this.lblSavingPath.Location = new System.Drawing.Point(37, 213);
+            this.lblSavingPath.Name = "lblSavingPath";
+            this.lblSavingPath.Size = new System.Drawing.Size(0, 13);
+            this.lblSavingPath.TabIndex = 7;
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtStatus.Location = new System.Drawing.Point(40, 89);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(359, 38);
+            this.txtStatus.TabIndex = 8;
             // 
             // Form1
             // 
+            this.AcceptButton = this.btnAutoSave;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(451, 261);
-            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.lblSavingPath);
+            this.Controls.Add(this.btnSavingFolder);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.btnAutoSave);
+            this.Controls.Add(this.btnAutoCopy);
             this.Controls.Add(this.txtSvrPath);
             this.Controls.Add(this.btnDownload);
             this.Name = "Form1";
@@ -79,7 +137,13 @@
         private System.Windows.Forms.Button btnDownload;
         private System.Windows.Forms.SaveFileDialog sfdMain;
         private System.Windows.Forms.TextBox txtSvrPath;
-        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnAutoCopy;
+        private System.Windows.Forms.FolderBrowserDialog fbdMain;
+        private System.Windows.Forms.Button btnAutoSave;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button btnSavingFolder;
+        private System.Windows.Forms.Label lblSavingPath;
+        private System.Windows.Forms.TextBox txtStatus;
     }
 }
 
--- filedownloaderclient/filedownloaderclient/Form1.cs
+++ filedownloaderclient/filedownloaderclient/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -13,6 +14,9 @@ namespace filedownloaderclient
 {
     public partial class Form1 : Form
     {
+        string _autoSavingPath;
+        string _destinyPath;
+
         public Form1()
         {
             InitializeComponent();
@@ -22,23 +26,52 @@ namespace filedownloaderclient
         {
             if (sfdMain.ShowDialog() == DialogResult.OK)
             {
-                var svrPath = new Uri(txtSvrPath.Text);
+                var svrPath = txtSvrPath.Text;
                 var localPath = sfdMain.FileName;
-                var wc = new WebClient();
-                wc.DownloadProgressChanged += Wc_DownloadProgressChanged;
-                wc.DownloadFileCompleted += Wc_DownloadFileCompleted;
-                wc.DownloadFileAsync(svrPath, localPath);
-            }        
+                DownloadFile(svrPath, localPath);
+            }
+        }        
+
+        private void btnAutoCopy_Click(object sender, EventArgs e)
+        {
+            txtSvrPath.Text = Clipboard.ContainsText() ? Clipboard.GetText() :
+                  String.Empty;
+            txtFileName.Text = String.Empty;
+            txtFileName.Focus();
         }
 
-        private void Wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        private void btnSavingFolder_Click(object sender, EventArgs e)
         {
-            lblStatus.Text = $"{e.ProgressPercentage.ToString()}%";
+            if (fbdMain.ShowDialog() == DialogResult.OK)
+            {
+                _autoSavingPath = lblSavingPath.Text = fbdMain.SelectedPath;
+            }
         }
 
-        private void Wc_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        private void btnAutoSave_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("File downloaded");
+            if (!String.IsNullOrEmpty(_autoSavingPath))
+            {
+                var fileCountOnSavingFolder = Directory.GetFiles(_autoSavingPath).Count();
+                _destinyPath = $@"{_autoSavingPath}\{++fileCountOnSavingFolder}. {txtFileName.Text}.mp4";
+                DownloadFile(txtSvrPath.Text, _destinyPath);
+            }
+            else MessageBox.Show("Set the autosaving folder first");
         }
+
+        private void DownloadFile(string origin, string destinyName)
+        {
+            var svrPath = new Uri(origin);
+            var wc = new WebClient();
+            wc.DownloadProgressChanged += Wc_DownloadProgressChanged;
+            wc.DownloadFileCompleted += Wc_DownloadFileCompleted;
+            wc.DownloadFileAsync(svrPath, destinyName);
+        }
+
+        private void Wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e) =>
+            txtStatus.Text = $"{e.ProgressPercentage.ToString()}%";
+
+        private void Wc_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e) =>
+            txtStatus.Text = $"{_destinyPath} Download Finished";
     }
 }
